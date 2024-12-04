933e403a6121dd0dbc2ae0872c36bdc6d569f728
Chris Johnson <johnsoctbus@gmail.com>
2024-12-04 04:51:27 -0500

    advent of code repo

---
+++ 1/go.mod
@@ -0,0 +1,3 @@
+module github.com/Johnsoct/adventofcode/1
+
+go 1.23.3
---
+++ 1/main.go
@@ -0,0 +1,10 @@
+// Solution for Advent of Code 2024 - Day one; puzzle one
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Day one! Les get it!")
+}
