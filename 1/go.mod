5811c88d593b51ce8f1afcd370f82260ffaa5fc0
Chris Johnson <johnsoctbus@gmail.com>
2024-12-04 07:56:30 -0500

    wip - need to process input into file and use file to prevent unnecessarily hitting AOC servers

--- 1/go.mod
+++ 1/go.mod
@@ -1,3 +1,5 @@
 module github.com/Johnsoct/adventofcode/1
 
 go 1.23.3
+
+require github.com/joho/godotenv v1.5.1
--- 1/main.go
+++ 1/main.go
@@ -3,8 +3,73 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func getNewHTTPClient() *http.Client {
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	return client
+}
+
+func getNewRequest(url string) *http.Request {
+	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "formRequest(): error creating new request: %v", err)
+	}
+
+	r.AddCookie(getSessionCookie())
+
+	return r
+}
+
+func getSessionCookie() *http.Cookie {
+	cookie := &http.Cookie{
+		Name:  "session",
+		Value: os.Getenv("AOC_TOKEN"),
+	}
+
+	return cookie
+}
+
+func getPuzzleInput(url string) []byte {
+	client := getNewHTTPClient()
+	req := getNewRequest(url)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not GET %s", url)
+	}
+	defer resp.Body.Close()
+
+	input, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "getPuzzleInput(): error reading response body, %v", err)
+	}
+
+	return input
+}
+
+func writeInputFile(input []byte) {
+
+}
 
 func main() {
 	fmt.Println("Day one! Les get it!")
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ENV's not loaded")
+	}
+
+	getPuzzleInput("https://adventofcode.com/2024/day/1/input")
+	// writeInputFile(input)
 }
